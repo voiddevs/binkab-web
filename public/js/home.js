14aefbd2b0c7189ac0a5ddeb8c6dcfabc372f9d7
Raul Armando Paez <raul.armando,paez@gmail.com>
2019-03-11 23:05:07 -0700

    Slider
    Se agrego slider

---
+++ public/js/home.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    var SwiperHome = new Swiper ('.swiper-container-home', {
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 1,
+        effect: 'fade',
+        autoplay: {
+            delay: 5000,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      })
+  });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,8 @@
     <title>Home - BinKab</title>
 
     <link rel="stylesheet" href="{{asset('css/menu.css')}}">
+    <link rel="stylesheet" href="{{asset('css/swiper.css')}}">
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
 </head>
 <body class="is-preload">
     <!-- Wrapper -->
@@ -15,32 +17,58 @@
         <!-- Header -->
         <header id="header">
             <div class="inner">
-                    <!-- Nav -->
-                    <nav>    
-                        <ul>
-                            <li><a href="#menu">Menu</a></li>
-                        </ul>
-                    </nav>
+                <div>
+                    <a href="/"><img src="{{asset('img/logo_blanco.png')}}" alt="logo-binkab"></a>
+                </div>
+                <!-- Nav -->
+                <nav>    
+                    <ul>
+                        <li><a href="#menu">Menu</a></li>
+                    </ul>
+                </nav>
             </div>
         </header>
 
         <!-- Menu -->
-            <nav id="menu">
-                <h2>Menu</h2>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="#">Áreas de deporte</a></li>
-                    <li><a href="#">Cultura</a></li>
-                    <li><a href="#">Turismo</a></li>
-                    <li><a href="#">Comida</a></li>
-                    <li><a href="#">Entretenimiento</a></li>
+        <nav id="menu">
+            <h2>Menu</h2>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="#">Áreas de deporte</a></li>
+                <li><a href="#">Cultura</a></li>
+                <li><a href="#">Turismo</a></li>
+                <li><a href="#">Comida</a></li>
+                <li><a href="#">Entretenimiento</a></li>
+            </ul>
+        </nav>
 
-                </ul>
-            </nav>
+        <!-- Swiper-Home -->
+        <div class="swiper-container-home">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <div class="slides-home" style="background-image: url({{asset('img/slides/slide-1.jpg')}});"></div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="slides-home" style="background-image: url({{asset('img/slides/slide-2.jpeg')}});"></div> 
+                </div>
+                <div class="swiper-slide">
+                    <div class="slides-home" style="background-image: url({{asset('img/slides/slide-3.jpg')}});"></div>
+                </div>
+                ...
+            </div>
+            <!-- Pagination -->
+            <div class="swiper-pagination"></div>
+        </div>
+
+
+
     </div>
     <script src="{{asset('js/browser.min.js')}}"></script>
     <script src="{{asset('js/jquery.min.js')}}"></script> 
     <script src="{{asset('js/breakpoints.min.js')}}"></script>
+    <script src="{{asset('js/swiper.js')}}"></script>
     <script src="{{asset('js/menu.min.js')}}"></script>
+    <script src="{{asset('js/home.js')}}"></script>
 </body>
 </html>
