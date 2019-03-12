5b018820533d1d74a628c53e01fc76dba18a5f70
Raul Armando Paez <raul.armando,paez@gmail.com>
2019-03-11 20:39:14 -0700

    Menu
    Se hizo estructura de home y se agrego una parte del menu

---
+++ resources/views/home.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home - Binkab</title>
+
+    <link rel="stylesheet" href="{{asset('css/menu.css')}}">
+</head>
+<body class="is-preload">
+    <!-- Wrapper -->
+	<div id="wrapper">
+        
+        <!-- Header -->
+        <header id="header">
+            <div class="inner">
+                    <!-- Nav -->
+                        <nav>
+                            
+                            <ul>
+                                <li><a href="#menu">Menu</a></li>
+                            </ul>
+                        </nav>
+                </div>
+            </header>
+
+        <!-- Menu -->
+            <nav id="menu">
+                <h2>Menu</h2>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="#">Áreas de deporte</a></li>
+                    <li><a href="#">Cultura</a></li>
+                    <li><a href="#">Turismo</a></li>
+                    <li><a href="#">Comida</a></li>
+                    <li><a href="#">Entretenimiento</a></li>
+
+                </ul>
+            </nav>
+    </div>
+    <script src="{{asset('js/browser.min.js')}}"></script>
+    <script src="{{asset('js/jquery.min.js')}}"></script> 
+    <script src="{{asset('js/breakpoints.min.js')}}"></script>
+    <script src="{{asset('js/menu.min.js')}}"></script>
+</body>
+</html>
